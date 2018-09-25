ec73e260f0754d015824c1f177f363b1b04c1953
hanashiryuta <kbrn.kabirunrun@gmail.com>
2018-09-26 00:47:54 +0900

    フォルダ作成

---
+++ Team27Project/Assets/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Rigidbody2D rigid;
+    public float moveSpeed;
+    public float moveForceMultiplier = 50;
+    public float jumpPower;
+    bool isJumpFlag;
+
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        Move();
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
+    }
+
+    /// <summary>
+    /// 移動処理
+    /// </summary>
+    public void Move()
+    {
+        Vector2 moveVector = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, 0);
+        Vector2 rigidVelocity = new Vector2(rigid.velocity.x, 0);
+        rigid.AddForce(moveForceMultiplier * (moveVector - rigidVelocity));
+    }
+
+    public void Jump()
+    {
+        if(isJumpFlag)
+        {
+            isJumpFlag = false;
+            rigid.AddForce(Vector2.up * jumpPower);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumpFlag = true;
+        }
+    }
+
+}
---
+++ Team27Project/Assets/TestFloorSet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFloorSet : MonoBehaviour {
+
+    public GameObject originFloorBlock;
+    public int blockStartPositionX;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < 20; i++)
+        {
+            Instantiate(originFloorBlock, new Vector2(blockStartPositionX + i, 0), Quaternion.identity);
+
+            Instantiate(originFloorBlock, new Vector2(blockStartPositionX + i, 5), Quaternion.identity);
+        }
+
+        for (int i = 0; i < 6; i++)
+        {
+            Instantiate(originFloorBlock, new Vector2(-1, 0 + i), Quaternion.identity);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
